b00aa72e06624ba441472856939c3a02f6f45463
munjeli <1256674+munjeli@users.noreply.github.com>
2024-01-23 08:55:48 -0600

    feat: added dbs to tf

--- tunnelbot-project.tf
+++ tunnelbot-project.tf
@@ -3,7 +3,7 @@ resource "google_project" "tunnelbot" {
 
   project_id = "tunnelbot-${var.color}"
   name       = "tunnelbot-${var.color}"
-  org_id     =  var.org_id
+  org_id     = var.org_id
 
   labels = {
     "firebase" = "enabled"
@@ -16,8 +16,8 @@ resource "google_firebase_project" "tunnelbot_firebase_project" {
 }
 
 resource "google_firebase_web_app" "tunnelbot_web_app" {
-  provider = google-beta
-  project = google_project.tunnelbot.project_id
+  provider     = google-beta
+  project      = google_project.tunnelbot.project_id
   display_name = google_project.tunnelbot.name
 }
 
@@ -32,23 +32,48 @@ resource "google_storage_bucket" "tunnelbot_bucket" {
   location = "US"
 }
 
+resource "google_firestore_database" "app_database" {
+  project                           = google_project.tunnelbot.project_id
+  name                              = google_project.tunnelbot.name
+  location_id                       = "us-central"
+  type                              = "FIRESTORE_NATIVE"
+  concurrency_mode                  = "OPTIMISTIC"
+  app_engine_integration_mode       = "DISABLED"
+  point_in_time_recovery_enablement = "POINT_IN_TIME_RECOVERY_ENABLED"
+  delete_protection_state           = "DELETE_PROTECTION_ENABLED"
+  deletion_policy                   = "DELETE"
+}
+
 resource "google_storage_bucket_object" "tunnelbot_config" {
   provider = google-beta
-  bucket = google_storage_bucket.tunnelbot_bucket.name
-  name = "firebase-config.json"
+  bucket   = google_storage_bucket.tunnelbot_bucket.name
+  name     = "firebase-config.json"
 
   content = jsonencode({
-    appId              = google_firebase_web_app.tunnelbot_web_app.app_id
-    apiKey             = data.google_firebase_web_app_config.tunnelbot_config.api_key
-    authDomain         = data.google_firebase_web_app_config.tunnelbot_config.auth_domain
-    databaseURL        = lookup(data.google_firebase_web_app_config.tunnelbot_config "database_url", "")
-    storageBucket      = lookup(data.google_firebase_web_app_config.tunnelbot_config "storage_bucket", "")
-    messagingSenderId  = lookup(data.google_firebase_web_app_config.tunnelbot_config "messaging_sender_id", "")
-    measurementId      = lookup(data.google_firebase_web_app_config.tunnelbot_config "measurement_id", "")
+    appId             = google_firebase_web_app.tunnelbot_web_app.app_id
+    apiKey            = data.google_firebase_web_app_config.tunnelbot_config.api_key
+    authDomain        = data.google_firebase_web_app_config.tunnelbot_config.auth_domain
+    databaseURL       = lookup(data.google_firebase_web_app_config.tunnelbot_config, "database_url", google_firestore_database.app_database.id)
+    storageBucket     = lookup(data.google_firebase_web_app_config.tunnelbot_config.storage_bucket, "storage_bucket", google_storage_bucket.tunnelbot_bucket.name)
+    messagingSenderId = lookup(data.google_firebase_web_app_config.tunnelbot_config, "messaging_sender_id", "")
+    measurementId     = lookup(data.google_firebase_web_app_config.tunnelbot_config, "measurement_id", "")
   })
 }
 
+resource "google_firestore_backup_schedule" "daily-backup" {
+  project = google_project.tunnelbot.project_id
 
+  retention = "604800s" // 7 days (maximum possible value for daily backups)
 
+  daily_recurrence {
+    hour = 0
+  }
+}
 
-
+resource "google_firebase_database_instance" "curtains" {
+  provider = google-beta
+  project  = google_project.tunnelbot.project_id
+  region   = "us-central1"
+  instance_id = google_project.tunnelbot.name
+  desired_state   = "DISABLED"
+}
